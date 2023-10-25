2b98ef182d4958d4dba051524f6cc6fc6a8dd129
duongtrinh295 <duongtrinhbg2003@gmail.com>
2023-10-26 04:24:42 +0700

    add Entity

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Proecjt Asp.net core 3.1
+## Install packages
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.EntityFrameworkCore.Tools
 ## How to configure and run
 -  Say hi
---
+++ eShopSolution.Data/EF/EShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using eShopSolution.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+	public class EShopDbContext :DbContext
+	{
+		public EShopDbContext( DbContextOptions options) : base(options)
+		{
+			
+		}  
+
+		public DbSet<Product> Products { set; get; }
+		public DbSet<Category> Categories { set; get; }
+
+	}
+}
+
