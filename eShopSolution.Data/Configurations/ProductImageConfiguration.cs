2b794fe3d39608a2d6ab306aec683a549a7c96fd
duongtrinh295 <duongtrinhbg2003@gmail.com>
2023-11-05 10:06:48 +0700

    Add product image table by migration

---
+++ eShopSolution.Data/Configurations/ProductImageConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using eShopSolution.Data.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+	public class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+	{
+		public void Configure(EntityTypeBuilder<ProductImage> builder)
+		{
+			builder.ToTable("ProductImage");
+			builder.HasKey(x => x.Id);
+
+			builder.Property(x => x.Id).UseIdentityColumn();
+
+			builder.Property(x => x.ImagePath).HasMaxLength(200).IsRequired(true);
+			builder.Property(x => x.Caption).HasMaxLength(200);
+
+
+			builder.HasOne(x => x.Product).WithMany(x => x.ProductImages).HasForeignKey(x => x.ProductId);
+
+		}
+	}
+}
